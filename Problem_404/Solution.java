ca2fbf626ec6eba4004c4a5307d793fbbc158631
akirad <test@example.com>
2020-08-28 12:08:04 +0900

    Delete the inner class definition because it's not necessary.

--- Problem_404/Solution.java
+++ Problem_404/Solution.java
@@ -25,17 +25,4 @@ class Solution {
         }
         return sum;
     }
-
-    static private class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
 }
